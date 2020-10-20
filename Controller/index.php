ddfb5640d57f4a9069de8ba1936814de0fa20ce6
Lea Dalibard <leadalibard@gmail.com>
2020-10-20 10:23:25 +0200

    Changing path and view

--- Controller/index.php
+++ Controller/index.php
@@ -5,7 +5,10 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-//require '../Model/database.php';
+require '../config.php';
+require '../Model/database.php';
+
+//$pdo = openConnection();
 
 
 
--- Model/database.php
+++ Model/database.php
@@ -5,7 +5,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require 'config.php';
 
 function openConnection(): PDO
 {
